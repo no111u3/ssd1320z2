3711079440b12c2cbd6b179233368b3f9c425701
Boris Vinogradov <no111u3@gmail.com>
2021-06-18 19:02:45 +0300

    Add draft proto for display structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
 #![no_std]
-
 //! Generic SPI interface for display drivers
+
+mod command;
+mod display;
+mod error;
+
+/// Variant for Surenoo SUR383S1000WG01
+/// Two SSD1320 controllers on board with single interface
+#[derive(Copy, Clone, Debug)]
+struct Ssd1320z2 {}
+
+impl Ssd1320z2 {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
